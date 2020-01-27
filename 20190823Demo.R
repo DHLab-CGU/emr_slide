4e023004ba3af0f56f42eca9075438ec1958a136
yijutseng <beergt@gmail.com>
2020-01-27 10:27:11 -0800

    Update 20190823Demo.R

--- 20190823Demo.R
+++ 20190823Demo.R
@@ -9,8 +9,32 @@ library(emr)
 ####################################################
 # Use case 1
 ####################################################
+selectedPDA <- 
+  selectCases(sample_MIMICIII, 
+              SUBJECT_ID, ICD9_CODE, 
+              ADMITTIME, "2015/10/01", 
+              ICD, caseCondition = "^7470", 
+              caseCount = 1, 
+              CaseName = "PDA")
+selectedPDA
 
+elix <- 
+  groupedDataLongToWide(
+    sample_MIMICIII,SUBJECT_ID,
+    ICD9_CODE,ADMITTIME, 
+    "9999-10-01", groupDataType = Elix, 
+    isDescription = T, 
+    selectedCaseFile = selectedPDA)
 
+library(tableone)
+var_col <- 
+  names(elix)[2:(length(elix)-1)]
+table_Elix <- 
+  CreateTableOne(vars = var_col,
+                 strata = "selectedCase", 
+                 data = as.data.frame(elix), 
+                 factorVars = var_col)
+table_Elix
 ####################################################
 # Closer look
 ####################################################
